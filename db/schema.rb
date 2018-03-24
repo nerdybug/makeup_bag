b6cf39a366a09bb5d17d48500ab36bda4356746c
Meredith Reece <mardebugger@gmail.com>
2018-03-24 01:52:05 +0000

    add type to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324011410) do
+ActiveRecord::Schema.define(version: 20180324015032) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180324011410) do
     t.integer "brand_id"
     t.boolean "favorite"
     t.boolean "need_more"
+    t.string "type"
   end
 
   create_table "users", force: :cascade do |t|
