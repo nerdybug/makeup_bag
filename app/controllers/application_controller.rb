b3ffd1bf1d32d9b42e43e56e7cb0649cad8097a4
Meredith Reece <nerdybug@gmail.com>
2018-03-23 20:05:39 -0400

    add login routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,18 @@ class ApplicationController < Sinatra::Base
     session[:user_id] = @user.id
     erb :'users/show'
   end
+  
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'users/show'
+    else
+      redirect '/login'
+    end
+  end
 end
