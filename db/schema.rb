cad2ccc1d64b775d41b8a85d2b01e72b3bfb027b
Meredith Reece <mardebugger@gmail.com>
2018-03-24 02:56:51 +0000

    change name of type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324015032) do
+ActiveRecord::Schema.define(version: 20180324024157) do
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 20180324015032) do
     t.integer "brand_id"
     t.boolean "favorite"
     t.boolean "need_more"
-    t.string "type"
+    t.string "type_of_item"
   end
 
   create_table "users", force: :cascade do |t|
