9a4281cc9985ddcaf892f7f8ba37d6603b591588
Meredith Reece <mardebugger@gmail.com>
2018-03-30 01:12:30 +0000

    add item routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,16 @@
 class ItemsController < ApplicationController
 
+  get '/items/add' do
+    @user = User.find_by(id: session[:user_id])
+    erb :'items/add'
+  end
+
+  post '/items' do
+    @user = User.find_by(id: session[:user_id])
+    @item = Item.create(params[:item])
+    @item.user_id = @user.id
+    @item.save
+    binding.pry
+    erb :'users/show'
+  end
 end
