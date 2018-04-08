9a2bdccd9b6306cc7a2692597a5cab2f8a278876
Meredith Reece <mardebugger@gmail.com>
2018-04-08 16:52:25 +0000

    remove old code

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,21 +28,6 @@ class ItemsController < ApplicationController
     @item = Item.find_by(id: params[:id])
     params[:item].each {|k,v| @item.update("#{k}": "#{v}") if v != "" && k != "favorite" && k != "need_more"}
 
-    # if params[:item][:name] != ""
-    # 	@item.update(name: params[:item][:name])
-    # end
-    # if params[:item][:type_of_item] != ""
-    # 	@item.update(type_of_item: params[:item][:type_of_item])
-    # end
-    # if params[:item][:color] != ""
-    # 	@item.update(color: params[:item][:color])
-    # end
-    # if params[:item][:favorite] != ""
-    #   @item.update(favorite: params[:item][:favorite])
-    # end
-    # if params[:item][:need_more] != ""
-    #   @item.update(need_more: params[:item][:need_more])
-    # end
     redirect "/items/#{@item.id}"
   end
 end
