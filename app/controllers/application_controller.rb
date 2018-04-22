902c7e576efcade50b925fae2b6a19db944ef0c5
Meredith Reece <mardebugger@gmail.com>
2018-04-22 16:06:06 +0000

    remove routes with user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,50 +15,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/signup' do
-    erb :'users/signup'
-  end
-
-  post '/signup' do
-    @taken = User.find_by(username: params[:username])
-    if @taken
-      erb :'users/taken'
-    else
-      @user = User.create(params)
-      session[:user_id] = @user.id
-      erb :'users/show'
-    end
-  end
-
-  get '/login' do
-    erb :'users/login'
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      redirect '/bag'
-    else
-      redirect '/login'
-    end
-  end
-
-  get '/bag' do
-    @user = User.find_by(id: session[:user_id])
-    erb :'users/show'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect '/'
-  end
-
-  get '/bag' do
-    @user = User.find_by(id: session[:user_id])
-    erb :'users/show'
-  end
-
   helpers do
     def strip_string_params(params_hash)
     	params_hash.each do |k,v|
