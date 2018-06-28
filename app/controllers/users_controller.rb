ab465d42a79b6cb4822e4e103490ac14552e24c8
Meredith Reece <mardebugger@gmail.com>
2018-06-28 23:40:49 +0000

    last commit should have read: paste users helper methods from app controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -71,4 +71,30 @@ class UsersController < ApplicationController
     @user = get_user
     erb :'users/show'
   end
+
+  helpers do
+    def get_brand_name(brand_id)
+      Brand.find_by(id: brand_id).name
+    end
+
+    def collect_names(items_array) # return array with the brand names of each item
+      @names = []
+      items_array.each {|item| @names << get_brand_name(item.brand_id)}
+      @names
+    end
+
+    def items_ordered_by(names_in_order) # return array of brand names in either asc or desc order
+      @new = []
+      names_in_order.each do |name|
+        @brand = Brand.find_by(name: name)
+        @result = @items.select {|item| item.brand_id == @brand.id}
+        if !@result.is_a? Array
+          @new << @result
+        else
+          @result.each {|item| @new << item if !@new.include?(item)}
+        end
+      end
+      @new
+    end
+  end
 end
